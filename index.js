87ba4ec5320f0152e1d114b819096f80339eb486
SaltyTeamSupport <saltyteamsupport@gmail.com>
2026-06-18 16:53:25 -0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+    <script>
+        function avisar() {
+            window.open("https://saltyteamsupport.github.io/Apoiar-Path-of-ruins-entre-traumas-e-ilus-es/apoiar/", "_blank");
+            //alert("Obrigado pelo seu apoio ao path of ruins!");
+        }
+    </script>
