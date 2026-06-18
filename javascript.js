0c2f2ed40402ba10030f65495b97f2a72d9b09c2
SaltyTeamSupport <saltyteamsupport@gmail.com>
2026-06-18 17:13:38 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,6 +1,4 @@
-    <script>
-        function avisar() {
+       function avisar() {
             window.open("https://saltyteamsupport.github.io/Apoiar-Path-of-ruins-entre-traumas-e-ilus-es/apoiar/", "_blank");
             //alert("Obrigado pelo seu apoio ao path of ruins!");
         }
-    </script>
